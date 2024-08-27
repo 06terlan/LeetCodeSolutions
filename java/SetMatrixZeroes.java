d007dafba5c8c5bd940dcf13196ad203d895d352
Tarlan Abdullayev <terlan.abd@gmail.com>
2024-08-27 00:32:21 -0700

    Set Matrix Zeroes

--- java/SetMatrixZeroes.java
+++ java/SetMatrixZeroes.java
@@ -7,5 +7,43 @@ package java;
  * @see https://leetcode.com/problems/set-matrix-zeroes/description/
  */
 public class SetMatrixZeroes {
-    
+    public void setZeroes(int[][] matrix) {
+        boolean firstRowHasZero = false, firstColHasZero = false;
+        
+        for (int i=0;i<matrix[0].length;i++) {
+            if (matrix[0][i] == 0) firstRowHasZero = true;
+        }
+
+        for (int i=0;i<matrix.length;i++) {
+            if (matrix[i][0] == 0) firstColHasZero = true;
+        }
+
+        for (int i=1;i<matrix.length;i++) {
+            for (int j=1;j<matrix[i].length;j++) {
+                if (matrix[i][j] == 0) {
+                    matrix[0][j] = 0;
+                    matrix[i][0] = 0;
+                }
+            }
+        }
+
+        for (int i=1;i<matrix.length;i++) {
+            for (int j=1;j<matrix[i].length;j++) {
+                if (matrix[0][j] == 0 || matrix[i][0] == 0) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+
+        if (firstRowHasZero) {
+            for (int i=0;i<matrix[0].length;i++) {
+                matrix[0][i] = 0;
+            }
+        }
+        if (firstColHasZero) {
+            for (int i=0;i<matrix.length;i++) {
+                matrix[i][0] = 0;
+            }
+        }
+    }
 }
